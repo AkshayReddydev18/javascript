43f85f2fb315b624c90fa3ee01dcd41a453f4aa5
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-08 11:25:12 +0530

    java

---
+++ ak.js
@@ -0,0 +1,33 @@
+
+// let a=10 ,  b = 10;
+
+
+// console.log(a+b);
+// console.log(typeof undefined);
+
+
+// let k=10;
+// let m='hello';
+// console.log(typeof (m,k));
+
+
+//  let obj={
+//     city:"hyd",
+//     country:"india",
+//     gender:"male",
+//     skill:["html","css","js",["sus","node.js","react","exp.js"]],
+//     address:{
+//       road_no:"2",
+//       street:"kphp",
+//       city:"hyd"}
+//     }
+// console.log(`my address is ${obj.address.street} at ${obj.address.city}`)
+// console.log("my address is "+obj.address.street+" at "+ obj.address.city)
+
+
+
+ let x=+prompt("enter a number");
+ let y=+prompt("enter a number");
+ let z=x+y;
+ alert("answer is"+z);
+ 
