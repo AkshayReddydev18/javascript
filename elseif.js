7fc6faa9d024d9b8521f99a028813f5a12b34b8e
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-08 13:26:06 +0530

    pri

---
+++ elseif.js
@@ -0,0 +1,24 @@
+// let x = "admin" ;
+
+// if (x == "admin"){
+//   console.log("logged in as admin");
+// }
+// else if (x == "employee"){
+//   console.log("logged in as employee");
+// }
+// else if (x == "superadmin"){
+//   console.log("logged in as superadmin");
+// }
+// else if (x == "customer"){
+//   console.log("logged in as customer");
+// }
+// else if (x == "anything"){
+//   console.log("logged in as unauthorised user");
+// }
+
+
+for(i=40; i<=50; i++){
+  if(((i-40)%2)!==0){
+    console.log(`40x${i-40}=${40*(i-40)}`)
+  }
+}
---
+++ fizzbuzz.js
@@ -0,0 +1,21 @@
+for(let i=1; i<=20; i++){
+    if((i%2)==0 && i%3==0){
+        console.log(i+"fizzbuzz");
+    }
+    else if(i%2==0){
+        console.log(i+"fizz");
+    }
+    else if(i%3==0){
+        console.log(i+"buzz");
+    }
+    else if(i%5==0){
+        console.log(i+"div 5");
+    }
+    else{
+        console.log(i);
+    }
+    // for( let j=1; j<=100; j++)
+    // if(i%1==0 && i%i==0 && i%j==0){
+    //     console.log(i+"prime");
+    // }
+}
