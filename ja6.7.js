a2707d8ea213dad3c0909e55f6d44238fa5784f2
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-14 22:34:33 +0530

    alk

--- ja6.7.js
+++ ja6.7.js
@@ -1,16 +1,16 @@
-// let unit=10;
-// if(unit>0 && unit<=5){
-//     console.log(" no discount");
-// }
-// else if(unit>=6 && unit<=10){
-//     console.log(" 5% discount");
-// }
-// else if(unit>=11 && unit<=20){
-//     console.log(" 10% discount");
-// }
-// else if(unit>20){
-//     console.log(" 15% discount");
-// }
+let unit=10;
+if(unit>0 && unit<=5){
+    console.log(" no discount");
+}
+else if(unit>=6 && unit<=10){
+    console.log(" 5% discount");
+}
+else if(unit>=11 && unit<=20){
+    console.log(" 10% discount");
+}
+else if(unit>20){
+    console.log(" 15% discount");
+}
 function akshay(x, y) {
   for (item = x; item <= y; item++) {
     if (item % 2 == 0 && item % 3 == 0) {
