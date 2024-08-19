8a693972b1e64dc4baaf18d2e4868894f080b904
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-19 19:40:12 +0530

    akshay

--- 1208/min_max.js
+++ 1208/min_max.js
@@ -36,3 +36,23 @@ switch (coffee) {
     break;
 }
 console.log(coffeename)
+
+
+num = prompt("Enter a number: ")
+min=num[0],minInd=0,max=num[0],maxInd=0
+for(i=1;i<num.length;i++){
+    if(num[i]<min){
+        min=num[i]
+        minInd=i
+        
+    }
+    if(num[i]>max){
+        max=num[i]
+        maxInd=i
+        
+    }
+    
+}
+console.log("Minimum  and maxmium number in "+num+" is : "+min+ "," +max)
+console.log("sum of min and max number in "+num+"is :"+(Number(min)+Number(max)))
+console.log("min and max index positionis are:"+minInd+","+maxInd)
