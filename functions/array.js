999ae85fbccf50c4029c5c8f517f940780e2e0f3
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-21 14:30:55 +0530

    arr

---
+++ functions/array.js
@@ -0,0 +1,17 @@
+// let arr=["hello","hii","bye"]
+// for(i=0;i<arr.length;i++){
+//     if(arr[i].startsWith("h"))
+//         console.log(arr[i])
+// }
+
+// let arr=["hello","hii","bye"]
+// for(i=0;i<arr.length;i++){
+//     if(!arr[i].includes("o"))
+//         console.log(arr[i])
+// }
+
+
+let arr=["hello","hii","bye"]
+for(i=0;i<arr.length;i++){``
+    console.log(arr[i].charAt(0))
+}
