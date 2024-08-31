52ff1649e3ac7cce67e2230008fc7991e79b24b1
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-31 13:16:32 +0530

    even

--- 3008PS/PAIRS.js
+++ 3008PS/PAIRS.js
@@ -1,36 +1,25 @@
-function sep(x){
-    arr = x.split(" ").map(Number);
-    // console.log(arr);
-emp=[];
-uni=[];
-dup=[];
-res=0
-for(i of arr) {
-    if(!emp.includes(i)){
-        emp.push(i);
+function sep(x) {
+  arr = x.split(" ").map(Number);
+  emp = [];
+  dup = [];
+  res = 0;
+  for (i of arr) {
+    if (!emp.includes(i)) {
+      emp.push(i);
     }
-}
-// console.log(emp);
-for(i of emp){
-    c=0;
-    for(j of arr){
-        if(i==j){
-            c++;
-        }
+  }
+  for (i of emp) {
+    c = 0;
+    for (j of arr) {
+      if (i == j) {
+        c++;
+      }
     }
-    if(c==1){
-        uni.push(i);
-        // console.log(i,`->`,c)
-        
-            
-        }
-    
-    else{
-        dup.push(i);
-        // console.log(i,`->`,c)
-        res+=Math.floor(c/2)
+    if (c > 1) {
+      dup.push(i);
+      res += Math.floor(c / 2);
     }
+  }
+  console.log(res);
 }
-console.log(res)
-}
- sep("30 50 30 50 20 50 50 20 50 50 50")
+sep("30 50 30 50 20 50 50 20 50 50 50");
